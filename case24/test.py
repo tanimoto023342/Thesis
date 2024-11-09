1d6691a93cc555caa6d01d967763a34514b2899b
shusuke <ilu89377@kwansei.ac.jp>
2024-11-09 10:37:25 +0900

    new case

---
+++ case24/test.py
@@ -0,0 +1,10 @@
+list=[0]
+
+for j in range(5):
+    sum=0
+    for i in list:
+        sum+=i
+
+    print(sum)
+
+    list.append(1)
---
+++ case24/testref.py
@@ -0,0 +1,13 @@
+def printListSum():
+    sum=0
+    for i in list:
+        sum+=i
+
+    print(sum)
+
+list=[0]
+
+for j in range(5):
+    printListSum()
+
+    list.append(1)
